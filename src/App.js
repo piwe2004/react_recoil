07abb65763b26a380a52856ff9aebc84722452a5
origin <piwe2004@gmail.com>
2023-05-09 14:18:42 +0900

    리스트 생성 및 수정, 삭제, 완료 기능

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useRecoilValue } from "recoil";
 import "./App.css";
+import TodoItemCreator from "./components/TodoItemCreator";
+import { todoListState } from "./todoAtom";
+import TodoItem from "./components/TodoItem";
 
 
 
 function App() {
-
+	const todoList = useRecoilValue(todoListState);	
 	return (
 		<div className="App">
-			
+			<TodoItemCreator />
+			{
+				todoList.map((todoItem)=>(
+					<TodoItem key={todoItem.id} item={todoItem} />
+				))
+			}
 		</div>
 	);
 }
---
+++ src/components/TodoItemCreator.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { useSetRecoilState } from 'recoil';
+import { todoListState } from '../todoAtom';
+
+const TodoItemCreator = () => {
+    const [inputValue, setInputValue] = useState("");
+    const setTodoList = useSetRecoilState(todoListState)
+    const handleChange = (e) =>{
+        setInputValue(e.target.value);
+    }
+    const addItem = () =>{
+        setTodoList((oldTodoList) => [
+            ...oldTodoList,
+            {
+                id: getId(),
+                text: inputValue,
+                isComplete:false
+            }
+        ])
+        setInputValue('')
+    }
+    return (
+        <div>
+            <input type='text' value={inputValue} onChange={handleChange} />
+            <button onClick={addItem}>Add</button>
+        </div>
+    )
+}
+
+let id = 0;
+function getId (){
+    return id++
+}
+
+export default TodoItemCreator
